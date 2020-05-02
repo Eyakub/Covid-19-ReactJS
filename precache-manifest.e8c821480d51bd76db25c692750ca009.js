38bfda704a5008639cba88eea50370516fc00ce4
Eyakub Sorkar <eyakubsorkar@gmail.com>
2020-05-02 11:19:48 +0600

    Updates

---
+++ precache-manifest.e8c821480d51bd76db25c692750ca009.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0783b07810aece037ae69b9367bfbd77",
+    "url": "/Covid-19-ReactJS/index.html"
+  },
+  {
+    "revision": "ca8646438a583da07c3f",
+    "url": "/Covid-19-ReactJS/static/css/main.c08d613e.chunk.css"
+  },
+  {
+    "revision": "7e0b5d4e005a12477953",
+    "url": "/Covid-19-ReactJS/static/js/2.862e3fa5.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19-ReactJS/static/js/2.862e3fa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca8646438a583da07c3f",
+    "url": "/Covid-19-ReactJS/static/js/main.43b7593b.chunk.js"
+  },
+  {
+    "revision": "d9045f4ca5468d96889c",
+    "url": "/Covid-19-ReactJS/static/js/runtime-main.46fe528b.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19-ReactJS/static/media/covid-19.d7265326.png"
+  }
+]);
